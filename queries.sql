e93e709945d63f039cddd3b8fc23352f573b76b1
Alexey Chernoivan <CheAlex@users.noreply.github.com>
2017-09-27 09:31:00 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -11,7 +11,7 @@ SELECT
 FROM image_views
 GROUP BY user_id
 ORDER BY ANY_VALUE(created_at) DESC
-LIMIT 3
+LIMIT 50
 
 -- 4) какое фото сколько раз было открыто посетителем
 SELECT
@@ -23,7 +23,7 @@ INNER JOIN (
     SELECT DISTINCT user_id
         FROM image_views
         ORDER BY ANY_VALUE(created_at) DESC
-        LIMIT 3
+        LIMIT 50
 ) AS users ON users.user_id = image_views.user_id
 GROUP BY user_id, image_id
 
@@ -40,7 +40,7 @@ FROM (
     FROM image_views
     GROUP BY user_id
     ORDER BY ANY_VALUE(created_at) DESC
-    LIMIT 3
+    LIMIT 50
 ) AS tmp
 ORDER BY first_open DESC
 
@@ -55,7 +55,7 @@ FROM (
     FROM image_views
     GROUP BY user_id
     ORDER BY ANY_VALUE(created_at) DESC
-    LIMIT 3
+    LIMIT 50
 ) AS tmp
 ORDER BY last_open DESC
 
@@ -70,6 +70,6 @@ FROM (
     FROM image_views
     GROUP BY user_id
     ORDER BY ANY_VALUE(created_at) DESC
-    LIMIT 3
+    LIMIT 50
 ) AS tmp
 ORDER BY total_open DESC
